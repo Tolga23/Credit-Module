187dc148f665439e520e7bd84e9dcb382ed7bcab
Tolga Hardal <hardaltolga@gmail.com>
2024-12-20 02:22:48 +0300

    feat(repository): add role and user repositories

---
+++ src/main/java/com/bank/app/domain/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.bank.app.domain.repository;
+
+import com.bank.app.domain.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
