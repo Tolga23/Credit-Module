a72aef637e7ca03dc77a7a11e32d29fab2479cc5
Tolga Hardal <hardaltolga@gmail.com>
2024-12-20 02:15:45 +0300

    feat(model): add role,user,base entity classes

---
+++ src/main/java/com/bank/app/domain/model/User.java
@@ -0,0 +1,25 @@
+package com.bank.app.domain.model;
+
+import com.bank.app.domain.model.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roleName;
+}
