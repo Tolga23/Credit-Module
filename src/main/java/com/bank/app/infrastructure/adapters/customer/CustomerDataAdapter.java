52089d04acbb58e45ab412513b74a4acfddb83b4
Tolga Hardal <hardaltolga@gmail.com>
2024-12-21 03:32:57 +0300

    feat(customer): Implement methods and mapper

--- src/main/java/com/bank/app/infrastructure/adapters/customer/CustomerDataAdapter.java
+++ src/main/java/com/bank/app/infrastructure/adapters/customer/CustomerDataAdapter.java
@@ -2,6 +2,8 @@ package com.bank.app.infrastructure.adapters.customer;
 
 import com.bank.app.domain.model.customer.Customer;
 import com.bank.app.domain.port.CustomerPort;
+import com.bank.app.infrastructure.adapters.customer.entity.CustomerEntity;
+import com.bank.app.infrastructure.adapters.customer.mapper.CustomerEntityMapper;
 import com.bank.app.infrastructure.adapters.customer.repository.CustomerJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,19 +14,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CustomerDataAdapter implements CustomerPort {
     private final CustomerJpaRepository repository;
+    private final CustomerEntityMapper mapper;
 
     @Override
     public Customer save(Customer customer) {
-        return null;
+        CustomerEntity entity = mapper.toEntity(customer);
+        return mapper.toDomain(repository.save(entity));
     }
 
     @Override
     public Optional<Customer> findById(Long id) {
-        return Optional.empty();
+        return repository.findById(id)
+                .map(mapper::toDomain);
     }
 
     @Override
     public void update(Customer customer) {
-
+        repository.save(mapper.toEntity(customer));
     }
 }
